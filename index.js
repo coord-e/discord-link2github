d93b1b97137f1b4e9d0b66d903cec101e731d3ed
coord.e <me@coord-e.com>
2018-08-15 16:43:19 +0900

    Fix: Lint

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const rp = require('request-promise');
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const rp = require('request-promise')
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
 const chan2repo = new Map()
 
 client.on('message', async msg => {
-  if(msg.isMemberMentioned(client.user)) {
-  const match = msg.content.match(/"(.+\/.+)"/)
-  if (!match) {
-    msg.reply("Invalid")
-    return
+  if (msg.isMemberMentioned(client.user)) {
+    const match = msg.content.match(/"(.+\/.+)"/)
+    if (!match) {
+      msg.reply('Invalid')
+      return
     }
     const repo = match[1]
     chan2repo.set(msg.channel, repo)
@@ -22,21 +22,21 @@ client.on('message', async msg => {
   }
   const match = msg.content.match(/#(\d+)/)
   if (match) {
-   if(!chan2repo.has(msg.channel)) {
-    msg.reply("Please set repo first")
-    return
-   }
-  const issueorpr = match[1]
+    if (!chan2repo.has(msg.channel)) {
+      msg.reply('Please set repo first')
+      return
+    }
+    const issueorpr = match[1]
     try {
       const issueurl = `https://github.com/${chan2repo.get(msg.channel)}/issues/${issueorpr}`
       await rp(issueurl)
       msg.channel.send(issueurl)
-    } catch(e) {
+    } catch (e) {
       const prurl = `https://github.com/${chan2repo.get(msg.channel)}/pulls/${issueorpr}`
       await rp(prurl)
       msg.channel.send(prurl)
     }
   }
-});
+})
 
 client.login('token');
