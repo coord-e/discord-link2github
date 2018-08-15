75b637fc6f99705510fe072cfb699aee42ef537b
coord.e <me@coord-e.com>
2018-08-15 16:40:28 +0900

    Add: Implement some of features

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const rp = require('request-promise');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -5,9 +6,36 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+const chan2repo = new Map()
+
+client.on('message', async msg => {
+  if(msg.isMemberMentioned(client.user)) {
+  const match = msg.content.match(/"(.+\/.+)"/)
+  if (!match) {
+    msg.reply("Invalid")
+    return
+    }
+    const repo = match[1]
+    chan2repo.set(msg.channel, repo)
+    msg.reply(`set to ${repo}`)
+    return
+  }
+  const match = msg.content.match(/#(\d+)/)
+  if (match) {
+   if(!chan2repo.has(msg.channel)) {
+    msg.reply("Please set repo first")
+    return
+   }
+  const issueorpr = match[1]
+    try {
+      const issueurl = `https://github.com/${chan2repo.get(msg.channel)}/issues/${issueorpr}`
+      await rp(issueurl)
+      msg.channel.send(issueurl)
+    } catch(e) {
+      const prurl = `https://github.com/${chan2repo.get(msg.channel)}/pulls/${issueorpr}`
+      await rp(prurl)
+      msg.channel.send(prurl)
+    }
   }
 });
 
